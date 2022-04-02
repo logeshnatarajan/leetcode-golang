b4a588ca1778688658aa672fe23aa4b5ece8f4ae
logesh <90084493+logeshnatarajan@users.noreply.github.com>
2022-04-02 15:22:15 +0530

    Create lenghtOfLongestSubstring.go

---
+++ longestSubstring/lenghtOfLongestSubstring.go
@@ -0,0 +1,27 @@
+func lengthOfLongestSubstring(s string) int {
+	n := len(s)
+	var result int
+	charIndexMap := make(map[uint8]int)
+	var start int
+	for end := 0; end < n; end++ {
+        if duplicateIndex, isDuplicate := charIndexMap[s[end]]; isDuplicate{
+			result = max(result, end-start)
+			for i := start; i <= duplicateIndex; i++ {
+				delete(charIndexMap, s[i])
+			}
+			start = duplicateIndex + 1
+        }
+		charIndexMap[s[end]] = end
+	}
+	result = max(result, n-start)
+
+	return result
+}
+func max(n,m int)int{
+    if n>m{
+        return n
+    }else{
+        return m
+    }
+}
+
