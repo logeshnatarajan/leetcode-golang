b6d35f2276acdcd3dfe4d3a07f172b35befa3a59
logesh <90084493+logeshnatarajan@users.noreply.github.com>
2022-04-14 21:00:31 +0530

    Create removeElements.go

---
+++ Remove Linked List Elements/removeElements.go
@@ -0,0 +1,18 @@
+func removeElements(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return head
+	}
+	curr := head
+
+	for ;curr.Next!=nil; {
+		if curr.Next.Val==val {
+			curr.Next = curr.Next.Next
+		} else {
+			curr = curr.Next
+		}
+	}
+	if head.Val==val {
+		return head.Next
+	}
+	return head
+}
