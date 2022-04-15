24eea5f93f8e60eeda0d97c1d90f7c54a42ea4d3
logesh <90084493+logeshnatarajan@users.noreply.github.com>
2022-04-15 09:56:18 +0530

    Create isUgly.go

---
+++ Ugly Number/isUgly.go
@@ -0,0 +1,15 @@
+func isUgly(n int) bool {
+    if n<=0{
+        return false
+    }else if(n==1){
+        return true
+    }else if(n%2==0){  
+        return isUgly(n/2)
+    }else if(n%3==0){
+        return isUgly(n/3)
+    }else if(n%5==0){
+        return isUgly(n/5)
+    }else{
+        return false
+    }
+}
