52c3c541db6bac224ddbd06d4a120cab48ae1419
logesh <90084493+logeshnatarajan@users.noreply.github.com>
2022-04-17 15:52:13 +0530

    Create invertTree.go

---
+++ Invert Binary Tree/invertTree.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {    
+    dfs(root)
+    return root   
+}
+func dfs(root *TreeNode){
+    if root == nil{
+        return 
+    }   
+    if root.Left != nil{
+        dfs(root.Left)
+    }    
+    if root.Right != nil{
+        dfs(root.Right)
+    }    
+    root.Left, root.Right = root.Right, root.Left
+}
