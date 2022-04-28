8b568ad46a3926ee760ff408d13ce64ab2ecce74
logesh <90084493+logeshnatarajan@users.noreply.github.com>
2022-04-28 10:23:21 +0530

    Create sumOfLeftLeaves.go

---
+++ Sum of Left Leaves/sumOfLeftLeaves.go
@@ -0,0 +1,9 @@
+func sumOfLeftLeaves(root *TreeNode) int {
+    if root == nil {
+        return 0
+    } else if root.Left != nil && root.Left.Left == nil && root.Left.Right == nil {
+        return root.Left.Val + sumOfLeftLeaves(root.Right) 
+    } else {
+        return sumOfLeftLeaves(root.Left) + sumOfLeftLeaves(root.Right)
+    }
+}
