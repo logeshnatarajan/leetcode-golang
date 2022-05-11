2db0421087c4b4dba7e2e7a53c7016c0a8f521cd
logesh <90084493+logeshnatarajan@users.noreply.github.com>
2022-05-11 10:23:16 +0530

    Update hammingDistance.go

--- Hamming Distance/hammingDistance.go
+++ Hamming Distance/hammingDistance.go
@@ -1,1 +1,12 @@
+func hammingDistance(x int, y int) int {
+    xBin := fmt.Sprintf("%031b", x)
+    yBin := fmt.Sprintf("%031b", y)
+    ret := 0
+    for i:=0;i<31;i++ {
+        if xBin[i] != yBin[i] {
+            ret++
+        }
+    }
+    return ret
 
+}
