56ba50c238000599130eb6e9657f7aab7e2a44fd
logesh <90084493+logeshnatarajan@users.noreply.github.com>
2022-05-15 19:50:50 +0530

    Update licenseKeyFormatting.go

--- License Key Formatting/licenseKeyFormatting.go
+++ License Key Formatting/licenseKeyFormatting.go
@@ -19,3 +19,24 @@ func licenseKeyFormatting(s string, k int) string {
     }
     return string(res)
 }
+
+// another easy to understand solution
+
+func licenseKeyFormatting(s string, k int) string {
+    var r string
+    i:=0
+    for v:=len(s)-1;v>=0;v--{
+        if i==k{
+            r="-"+r
+            i=0
+        }
+        if s[v]!='-'{
+            r=string(s[v])+r
+            i++ 
+        }
+  
+    }
+    r=strings.ToUpper(r)
+    r= strings.TrimPrefix(r,"-")
+    return r
+}
