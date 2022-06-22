47e2b8d25844c6135e09749324e32ff276bcf073
logesh <90084493+logeshnatarajan@users.noreply.github.com>
2022-06-22 22:32:24 +0530

    Update findMaxAverage.go

--- Maximum Average Subarray I/findMaxAverage.go
+++ Maximum Average Subarray I/findMaxAverage.go
@@ -10,3 +10,27 @@ func findMaxAverage(nums []int, k int) float64 {
 	}
 	return max / float64(k)
 }
+
+// another solution 
+
+func max(a,b float64)float64{
+    if a > b{
+        return a
+    }
+    return b
+}
+func findMaxAverage(nums []int, k int) float64 {
+    maxAverage := float64(math.MinInt32)
+    sum := 0
+    for i:=0; i<k;i++{
+        sum += nums[i]
+    }
+    maxAverage = max(maxAverage,float64(sum)/float64(k))
+    for i:=k;i<len(nums);i++{
+        sum += nums[i]
+        sum -= nums[i-k]
+        maxAverage = max(maxAverage,float64(sum)/float64(k))
+    }
+    
+    return maxAverage
+}
