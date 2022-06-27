d72d163f738e8e0631905d1fd90959e88e89948e
logesh <90084493+logeshnatarajan@users.noreply.github.com>
2022-06-27 15:03:03 +0530

    Update validPalindrome.go

--- Valid Palindrome II/validPalindrome.go
+++ Valid Palindrome II/validPalindrome.go
@@ -1,22 +1,26 @@
-// func validPalindrome(s string) bool {
-// 	isPalindrome := func(s string, leftBound, rightBound int) bool {
-// 		for left, right := leftBound, rightBound; left < right; left, right = left+1, right-1 {
-// 			if s[left] != s[right] {
-// 				return false
-// 			}
-// 		}
-// 		return true
-// 	}
+// easiest solution and efficient solution
 
-// 	for left, right := 0, len(s)-1; left < right; left, right = left+1, right-1 {
-// 		if s[left] != s[right] {
-// 			return isPalindrome(s, left+1, right) || isPalindrome(s, left, right-1)
-// 		}
-// 	}
+func validPalindrome(s string) bool {
+	isPalindrome := func(s string, leftBound, rightBound int) bool {
+		for left, right := leftBound, rightBound; left < right; left, right = left+1, right-1 {
+			if s[left] != s[right] {
+				return false
+			}
+		}
+		return true
+	}
 
-// 	return true
-// }
+	for left, right := 0, len(s)-1; left < right; left, right = left+1, right-1 {
+		if s[left] != s[right] {
+			return isPalindrome(s, left+1, right) || isPalindrome(s, left, right-1)
+		}
+	}
 
+	return true
+}
+
+
+// fastest solution 
 
 func validPalindrome(s string) bool {
     left, right, valid := isPalindrome(s, 0, len(s) - 1)
