3d1ba56aadabb3b02d517d4c733cd2b0794cb909
logesh <90084493+logeshnatarajan@users.noreply.github.com>
2022-07-01 22:50:05 +0530

    Create sortArrayByParity.go

---
+++ Sort Array By Parity/sortArrayByParity.go
@@ -0,0 +1,6 @@
+func sortArrayByParity(A []int) []int {
+    for left, right := 0, 0; right < len(A); right++ {
+        if A[right] % 2 == 0 { A[left], A[right] = A[right], A[left]; left++ }
+    }
+    return A
+}
